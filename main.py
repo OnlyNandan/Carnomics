26fac7e54775e832e06b2edbf32c67743f6b1cc3
NandanR <>
2022-10-04 17:29:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,338 @@
+from termcolor import colored
+slay=colored("                              _.-='_-         _                           ---- \n                         _.-='   _-          | ||'''''''---._______     __.||\n             ___.===''''-.______-,,,,,,,,,,,,`-''----' '''''       '''''  __'\n      __.--''     __        ,'                   o \           __        [__|\n __-''=======.--''  ''--.=================================.--''  ''--.=======:\n]           : /        \ : |  Welcome To TOP SECRET |    : /        \ :      :\nV___________:|          |: |  Done By Nandan,Guneev |    :|          |:   _-'\n'V__________: \        / :_|          Ahmed         |    : \        / :__-'\n -----------'  '-____-'  `-------------------------------'  '-____-'\n","red","on_grey")
+print(slay)
+n=1
+while(n==1):
+    tex=colored("Hi,Welcome to TOP SECRET- Carnomics project","blue")
+    print(tex)
+    ok=colored("This Project has been done By Nandan, Guneev, Ahmed","red")
+    print(ok)
+    y = int(input(">>>Enter the Number of years you would like to use the car for: "))
+    car=colored("The cars avalible with us today are ","white")
+    print(car)
+    line=colored(" -------------------","cyan")
+    print(line)
+    honda=colored(" |>1>Honda Civic   |","cyan")
+    print(honda)
+    por=colored(" |>2>Porsche 911   |","cyan")
+    print(por)
+    do=colored(" |>3>Dodge Hellcat |","cyan")
+    print(do)
+    tes=colored(" |>4>Tesla Roadster| ","cyan")
+    print(tes)
+    mar=colored(" |>5>Maruti 800    |","cyan")
+    print(mar)
+    vol=colored(" |>6>Volkswagen t1 |","cyan")
+    print(vol)
+    ford=colored(" |>7>Ford 150      |","cyan")
+    lines=colored(" -------------------","cyan")
+    print(lines)
+    c=input(">>>Enter your Selection of car now: ")
+    c=c.lower()
+    if(c=="honda civic" or c=="1" or c=="honda"):
+        print("                      ___..............._")
+        print("             __.. ' _'.'''''''\\''''''''- .`-._")
+        print(" ______.-'         (_) |      \\           ` \\`-. _")
+        print("/_       --------------'-------\\---....______\\__`.`  -..___")
+        print("| T      _.----._              |               |          _.._`--. _")
+        print("| |    .' ..--.. `.            |               |       .'.---..`.     -._")
+        print("\_j   /  /  __  \  \           |               |      / /  __  \ \        `-.")
+        print(" _|  |  |  /  \  |  |          |               |     / |  /  \  | |          |")
+        print("|__\_j  |  \__/  |  L__________|_______________|_____j |  \__/  | L__________J")
+        print("     `'\ \      / ./__________________________________\ \      / /___________\"")
+        print("        `.`----'.'                                     `.`----'.'")
+        print("          `----'                                         `----")
+
+        v=10000
+        m=(v/100)*10
+        r=800
+        c="Honda Civic"
+        a=colored(c,"magenta")
+        print(a)
+        print("is great choice of car")
+    elif(c=="porsche 911" or c=="2" or c=="porsche" or c=="911"):
+        print("              ____----------- _____")
+        print("/~~~~~~~~~~/~_--~~~------~~~~~     \"")
+        print(" `---`\  _-~      |                   \"")
+        print("   _-~  <_         |                     \[]")
+        print(" / ___     ~~--[""] |      ________-------'_")
+        print("> /~` \    |-.   `\~~.~~~~~                _ ~ - _")
+        print(" ~|  ||\%  |       |    ~  ._                ~ _   ~ ._")
+        print("   `_//|_%  \      |          ~  .              ~-_   /\"")
+        print("          `--__     |    _-____  /\               ~-_ \/.")
+        print("               ~--_ /  ,/ -~-_ \ \/          _______---~/")
+        print("                   ~~-/._<   \ \`~~~~~~~~~~~~~     ##--~/")
+        print("                         \    ) |`------##---~~~~-~  ) )")
+        print("                          ~-_/_/                  ~~ ~~")
+        v=200000
+        m=(v/100)*10
+        r=2500
+        c="Porsche 911"
+        b=colored(c,"magenta")
+        print(b)
+        print("is great choice of car")
+
+    elif(c=="dodge hellcat" or c=="3" or c=="dodge"):
+        print("                    ___..................____")
+        print("           _..--''~_______   _____...----~~~\\")
+        print("       __.'    .-'~       \\~      [_`.7     \\")
+        print(" .---~~      .'            \\           __..--\\_")
+        print("/             `-._          \\   _...~~~_..---~  ~~~~~~~~~~~~--.._")
+        print("\              /  ~~~~~~----_\`-~_-~~__          ~~~~~~~---.._    ~--.__")
+        print(" \     _       |==            |   ~--___--------...__          `-   _.--'''|")
+        print("  \ __/.-._\   |              |            ~~~~--.  `-._ ___...--~~~_.'|_Y |")
+        print("   `--'|/~_\\  |              |     _____           _.~~~__..--~~_.-~~~.-~/")
+        print("     | ||| |\\_|__            |.../.----.._.        | Y |__...--~~_.-~  _/")
+        print("      ~\\\ || ~|..__---____   |||||  .'~-. \\       |_..-----~~~~   _.~~")
+        print("        \`-'/ /     ~~~----...|'''|  |/'_'\ \\   |~~'           __.~")
+        print("         `~~~'                 ~~-:  ||| ~| |\\  |        __..~~")
+        print("                                   ~~|||  | | \\/  _.---~~")
+        print("                                     \\\  //  | ~~~")
+        print("                                      \`-'/    ")
+        print("                                       `-''")
+        v=100000
+        m=(v/100)*10
+        r=3000
+        c="Dodge Hellcat"
+        p = colored(c, "magenta")
+        print(p)
+        print("is great choice of car")
+    elif(c=="tesla roadster"or c=="4" or c=="tesla" ):
+        print("                              _.-='_-         _                           ---- ")
+        print("                         _.-='   _-          | ||'''''''---._______     __.||")
+        print("             ___.===''''-.______-,,,,,,,,,,,,`-''----' '''''       '''''  __'")
+        print("      __.--''     __        ,'                   o \           __        [__|")
+        print(" __-''=======.--''  ''--.=================================.--''  ''--.=======:")
+        print("]           : /        \ : |                        |    : /        \ :      :")
+        print("V___________:|          |: |                        |    :|          |:   _-'")
+        print("'V__________: \        / :_|                        |    : \        / :__-'")
+        print(" -----------'  '-____-'  `-------------------------------'  '-____-'")
+        v=400000
+        m=(v/100)*10
+        r=4000
+        c="Tesla Roadster"
+        x = colored(c, "magenta")
+        print(x)
+        print("is great choice of car")
+    elif(c=="maruti 800" or c=="5" or c=="maruti" or c=="800"):
+        print("        _______")
+        print("       //  ||\ \"")
+        print(" _____//___||_\ \___")
+        print(" )  _          _    \"")
+        print(" |_/ \________/ \___|")
+        print("___\_/________\_/______")
+        v=20000
+        m=(v/100)*10
+        r=1000
+        c="Maruti 800"
+        yas = colored(c, "magenta") #2 y variable 1 being years and 1 this
+        print(yas)
+        print("is great choice of car")
+    elif(c=="volkswagen t1" or c== "6" or c=="volkswagen" or c=="t1"):
+        print("   ---------------------------.")
+        print(" `/""""/""""/|""|'|""||""|   ' \.")
+        print(" /    /    / |__| |__||__|      |")
+        print("/----------=====================|")
+        print("| \  /V\  /    _.               |")
+        print("|()\ \W/ /()   _            _   |")
+        print("|   \   /     / \          / \  |")
+        print("=C========C==_| ) |--------| ) _/")
+        print(" \_\_/        \_/          \_/")
+        v=20000
+        m=(v/100)*10
+        r=1000
+        c="Volkswagen T1"
+        bt = colored(c, "magenta")
+        print(bt)
+        print("is great choice of car")
+    elif(c=="ford 150" or c=="7" or c=="ford" or c=="150"):
+        print("                                (>|")
+        print("                                (>|======\\")
+        print("                           ________||____ `\\")
+        print("                       _-~~~~~~~~|~|~~~|~|  |\\")
+        print("                      /          | |   | |  ||`\\")
+        print("                    /_           | |   | |  ||  `\\")
+        print("        ____-------(_|___________|_|___| |  ||    ||               _-~~~|")
+        print("  _--~~~            |           =|       ||~~~~~~~~~~~~~~~~~~~~~~~~     |")
+        print(" |]                 |            |       ||                             |")
+        print(" |=   /~~~~~~~~~~\  |           /'       ||         /~~~~~~~~~~\        |")
+        print(":~~~~/'  _ ----- _ `\~~~~~~~~~~~~~~~~~~~~~||~~~~~~~/'  _ ----- _ `\~~~~~~~~|")
+        print("|   | _-~         ~-_|____________==______||______| _-~         ~-_| __--~")
+        print("`~~~~/    _-----_    \___________________//______/-/    _-----_    \~~")
+        print("    ;    / \ _ / \    .                           :    / \ _ / \    .")
+        print("    |   | -((*))- |   |                           |   | -((*))- |   |")
+        print("    |    \  / \  /    |                           |    \  / \  /    ,")
+        print("     \    ~-----~    /                             \    ~-----~    /")
+        print("      ~-_         _-~                               `-_         _-~")
+        print("         ~ ----- ~                                     ~ ----- ~ ")
+        v=50000
+        m=(v/100)*10
+        r=3000
+        c="Ford 150"
+        bc = colored(c, "magenta")
+        print(bc)
+        print("is great choice of car",end="\n\n")
+    else:
+        print("Please Enter a valid choice")
+        re=input(">>>Would you like to Enter Another Selection? Enter Yes or (y/n) if would like to:- ")
+        re=re.lower()
+        if(re=="yes" or re == "y"):
+            n=1
+            print("\n \n")
+            continue
+        else:
+            n=0
+            break
+
+    cost=v+m
+    monthly=y*12
+    monthly=r*monthly
+    
+    if(cost<=monthly):
+        print("---------------------------------------------------------------------")
+        print("It will cost $",cost,"to buy the car and $",monthly,"to rent the car.")
+        yokok=colored(" Hence it is better to Buy the car instead of renting it",attrs=["underline"])
+        print(yokok)
+        print("value of car:- $",v)
+        print("maintanence of car for 1st year:- $",m)
+        print("Cost to rent the car for", y,"Years is $",monthly)
+        print("---------------------------------------------------------------------",end="\n\n")
+        if(c=="honda civic"):
+            print("                      ___..............._")
+            print("             __.. ' _'.'''''''\\''''''''- .`-._")
+            print(" ______.-'         (_) |      \\           ` \\`-. _")
+            print("/_       --------------'-------\\---....______\\__`.`  -..___")
+            print("| T      _.----._              |               |          _.._`--. _")
+            print("| |    .' ..--.. `.            |               |       .'.---..`.     -._")
+            print("\_j   /  /  __  \  \           |               |      / /  __  \ \        `-.")
+            print(" _|  |  |  /  \  |  |          |               |     / |  /  \  | |          |")
+            print("|__\_j  |  \__/  |  L__________|_______________|_____j |  \__/  | L__________J")
+            print("     `'\ \      / ./__________________________________\ \      / /___________\"")
+            print("        `.`----'.'                                     `.`----'.'")
+            print("          `----'                                         `----")
+
+
+        elif(c=="porsche 911"):
+            print("              ____----------- _____")
+            print("/~~~~~~~~~~/~_--~~~------~~~~~     \"")
+            print(" `---`\  _-~      |                   \"")
+            print("   _-~  <_         |                     \[]")
+            print(" / ___     ~~--[""] |      ________-------'_")
+            print("> /~` \    |-.   `\~~.~~~~~                _ ~ - _")
+            print(" ~|  ||\%  |       |    ~  ._                ~ _   ~ ._")
+            print("   `_//|_%  \      |          ~  .              ~-_   /\"")
+            print("          `--__     |    _-____  /\               ~-_ \/.")
+            print("               ~--_ /  ,/ -~-_ \ \/          _______---~/")
+            print("                   ~~-/._<   \ \`~~~~~~~~~~~~~     ##--~/")
+            print("                         \    ) |`------##---~~~~-~  ) )")
+            print("                          ~-_/_/                  ~~ ~~")
+
+
+        elif(c=="dodge hellcat"):
+            print("                    ___..................____")
+            print("           _..--''~_______   _____...----~~~\\")
+            print("       __.'    .-'~       \\~      [_`.7     \\")
+            print(" .---~~      .'            \\           __..--\\_")
+            print("/             `-._          \\   _...~~~_..---~  ~~~~~~~~~~~~--.._")
+            print("\              /  ~~~~~~----_\`-~_-~~__          ~~~~~~~---.._    ~--.__")
+            print(" \     _       |==            |   ~--___--------...__          `-   _.--'''|")
+            print("  \ __/.-._\   |              |            ~~~~--.  `-._ ___...--~~~_.'|_Y |")
+            print("   `--'|/~_\\  |              |     _____           _.~~~__..--~~_.-~~~.-~/")
+            print("     | ||| |\\_|__            |.../.----.._.        | Y |__...--~~_.-~  _/")
+            print("      ~\\\ || ~|..__---____   |||||  .'~-. \\       |_..-----~~~~   _.~~")
+            print("        \`-'/ /     ~~~----...|'''|  |/'_'\ \\   |~~'           __.~")
+            print("         `~~~'                 ~~-:  ||| ~| |\\  |        __..~~")
+            print("                                   ~~|||  | | \\/  _.---~~")
+            print("                                     \\\  //  | ~~~")
+            print("                                      \`-'/    ")
+            print("                                       `-''")
+
+
+        elif(c=="tesla roadster"):
+            print("                              _.-='_-         _                           ---- ")
+            print("                         _.-='   _-          | ||'''''''---._______     __.||")
+            print("             ___.===''''-.______-,,,,,,,,,,,,`-''----' '''''       '''''  __'")
+            print("      __.--''     __        ,'                   o \           __        [__|")
+            print(" __-''=======.--''  ''--.=================================.--''  ''--.=======:")
+            print("]           : /        \ : |                        |    : /        \ :      :")
+            print("V___________:|          |: |                        |    :|          |:   _-'")
+            print("'V__________: \        / :_|                        |    : \        / :__-'")
+            print(" -----------'  '-____-'  `-------------------------------'  '-____-'")
+
+
+        elif(c=="maruti 800"):
+            print("        _______")
+            print("       //  ||\ \"")
+            print(" _____//___||_\ \___")
+            print(" )  _          _    \"")
+            print(" |_/ \________/ \___|")
+            print("___\_/________\_/______")
+
+        elif(c=="volkswagen t1"):
+            print("   ---------------------------.")
+            print(" `/""""/""""/|""|'|""||""|   ' \.")
+            print(" /    /    / |__| |__||__|      |")
+            print("/----------=====================|")
+            print("| \  /V\  /    _.               |")
+            print("|()\ \W/ /()   _            _   |")
+            print("|   \   /     / \          / \  |")
+            print("=C========C==_| ) |--------| ) _/")
+            print(" \_\_/        \_/          \_/")
+
+        elif(c=="ford 150"):
+            print("                                (>|")
+            print("                                (>|======\\")
+            print("                           ________||____ `\\")
+            print("                       _-~~~~~~~~|~|~~~|~|  |\\")
+            print("                      /          | |   | |  ||`\\")
+            print("                    /_           | |   | |  ||  `\\")
+            print("        ____-------(_|___________|_|___| |  ||    ||               _-~~~|")
+            print("  _--~~~            |           =|       ||~~~~~~~~~~~~~~~~~~~~~~~~     |")
+            print(" |]                 |            |       ||                             |")
+            print(" |=   /~~~~~~~~~~\  |           /'       ||         /~~~~~~~~~~\        |")
+            print(":~~~~/'  _ ----- _ `\~~~~~~~~~~~~~~~~~~~~~||~~~~~~~/'  _ ----- _ `\~~~~~~~~|")
+            print("|   | _-~         ~-_|____________==______||______| _-~         ~-_| __--~")
+            print("`~~~~/    _-----_    \___________________//______/-/    _-----_    \~~")
+            print("    ;    / \ _ / \    .                           :    / \ _ / \    .")
+            print("    |   | -((*))- |   |                           |   | -((*))- |   |")
+            print("    |    \  / \  /    |                           |    \  / \  /    ,")
+            print("     \    ~-----~    /                             \    ~-----~    /")
+            print("      ~-_         _-~                               `-_         _-~")
+            print("         ~ ----- ~                                     ~ ----- ~ ")
+
+
+    else:
+        print("\n\n")
+        print("---------------------------------------------------------------------")
+        print("It will cost $",cost,"to buy the car and $",monthly,"to rent the car. ")
+        yokokok = colored(" Hence it is better to Buy the car instead of renting it", attrs=["underline"])
+        print(yokokok)
+        print("value of car:- $",v)
+        print("maintanence of car for 1st year:- $",m)
+        print("Cost to rent the car for", y,"Years is $",monthly)
+        print("---------------------------------------------------------------------",end="\n\n")
+
+    ch=input(">>>Would you like to buy the car or rent the car:- ")
+    ch=ch.lower()
+    if(ch=="buy"):
+        save=monthly-v
+        print("Great choice you are saving Amount $",save)
+        print("Thank You for buying a car with us \n")
+
+
+    elif(ch=="rent"):
+
+        save=v-monthly
+        print("Great choice you are saving Amount $",save)
+        print("Thank You for renting a car with us \n")
+    else:
+        print("Wrong Choice Entered Please Re-Run the program")
+
+    re=input(">>>Would you like to buy/rent another car? enter yes or(y/n) if would like to:- ")
+    re=re.lower()
+    if(re=="yes" or re=="y"):
+        print("\n\n")
+        n=1
+    else:
+        n=0
+
